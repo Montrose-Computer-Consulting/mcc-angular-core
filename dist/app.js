ad51a27a06e063baacad5d2e0ba3616ea12ba3c8
Geoff Oslund <geoffreyoslund@gmail.com>
2015-08-03 19:09:21 -0600

    commit !

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,45 @@
-angularApp = angular.module('sjae-core',['ui.bootstrap']);
+angularApp = angular.module('sjae-core',['ui.bootstrap', 'ui.router']);
+
+angularApp.config(['$stateProvider','$locationProvider', '$httpProvider',
+  function($stateProvider, $locationProvider, $httpProvider) {
+	$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+	$locationProvider.html5Mode(true);
+    
+    $stateProvider.state('home', {
+    	url: '/',
+    	templateUrl: '/'
+    }).state('services', {
+    	url: '/services',
+    	templateUrl: '/services'
+    }).state('packages', {
+    	url: '/packages',
+    	templateUrl: '/packages'
+    }).state('profile', {
+    	url: '/profile',
+    	templateUrl: '/profile'
+    }).state('blog', {
+    	url: '/blog',
+    	templateUrl: '/blog',
+    });
+
+  }]).run(['$templateCache', '$http', '$state' ,function($templateCache, $http, $state) {
+  		
+  		// angular.forEach($state.get(), function(state, key) {
+  		// 	console.log(state);
+  		// 	if(typeof state.abstract === 'undefined' ) {
+  		// 		$http({
+  		// 			url: state.url
+  		// 		}).success(function(res) {
+        
+  		// 			$templateCache.put(state.templateUrl, res)	
+  		// 		})
+  				
+  		// 	}
+  		// })
+
+  }]).controller('carousel', function() {
+    
+  })
 angularApp.controller("CarouselCtrl", ["$scope","$element", function($scope, $element) {
     $scope.myInterval = 5000;
     $scope.noWrapSlides = false;
@@ -37,5 +78,27 @@ angular.module("template/carousel/carousel.html", []).run(["$templateCache", fun
 }]);
 
 angularApp.controller('MainController', ['$scope', '$element', function($scope, $element) {
-	console.log("here");
+	var body = angular.element(document).find('body');
+	body.css("display", "block");
+	
 }])
+
+angularApp.directive('blogPostsTemplate', function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'blogPostsTemplate.htm',
+		controller: function($scope, $http, $sce) {
+		console.log("bpt controller");
+			$http({
+				url: '/blog',
+				headers: {'mcc-json-request': 'true'}
+			}).success(function(res) {
+				$scope.blogPosts = res;
+
+				angular.forEach($scope.blogPosts.data, function(value, key) {
+					value.content_html = $sce.trustAsHtml(value.content_html);
+				})
+			});
+		}
+	}
+});
